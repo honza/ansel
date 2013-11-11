6c3061ec0942cb665cca3e2931435a6d790a7963
Honza Pokorny <me@honza.ca>
2013-11-11 20:45:28 +0100

    Add resources

---
+++ resources/public/js/app.js
@@ -0,0 +1,63 @@
+(function () {
+    'use strict';
+
+    var url = '/upload';
+
+    angular.module('demo', [
+        'blueimp.fileupload'
+    ])
+        .config([
+            '$interpolateProvider', '$locationProvider', '$httpProvider', 'fileUploadProvider',
+            function ($interpolateProvider, $locationProvider, $httpProvider, fileUploadProvider) {
+                delete $httpProvider.defaults.headers.common['X-Requested-With'];
+                fileUploadProvider.defaults.redirect = window.location.href.replace(
+                    /\/[^\/]*$/,
+                    '/cors/result.html?%s'
+                );
+                $interpolateProvider.startSymbol('[[');
+                $interpolateProvider.endSymbol(']]');
+            }
+        ])
+
+        .controller('DemoFileUploadController', [
+            '$scope', '$http', '$filter', '$window',
+            function ($scope, $http) {
+                $scope.options = {
+                    url: url
+                };
+            }
+        ])
+
+        .controller('FileDestroyController', [
+            '$scope', '$http',
+            function ($scope, $http) {
+                var file = $scope.file,
+                    state;
+                if (file.url) {
+                    file.$state = function () {
+                        return state;
+                    };
+                    file.$destroy = function () {
+                        state = 'pending';
+                        return $http({
+                            url: file.deleteUrl,
+                            method: file.deleteType
+                        }).then(
+                            function () {
+                                state = 'resolved';
+                                $scope.clear(file);
+                            },
+                            function () {
+                                state = 'rejected';
+                            }
+                        );
+                    };
+                } else if (!file.$cancel && !file._index) {
+                    file.$cancel = function () {
+                        $scope.clear(file);
+                    };
+                }
+            }
+        ]);
+
+}());
