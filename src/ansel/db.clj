c49d82794c44a9d3b009a66ad3277a8034d8e3db
Honza Pokorny <me@honza.ca>
2014-05-07 07:59:22 +0200

    Db spec generalization

--- src/ansel/db.clj
+++ src/ansel/db.clj
@@ -11,13 +11,6 @@
             [ansel.util :refer :all])
   (:import org.mindrot.jbcrypt.BCrypt))
 
-;; TODO: Generalize database configuration
-(def db-spec
-  {:classname "org.postgresql.Driver"
-   :subprotocol "postgresql"
-   :subname "//localhost:5432/ansel"
-   :user "honza"})
-
 (defqueries "sql/queries.sql")
 (def tnow (comp to-timestamp now))
 
@@ -28,9 +21,36 @@
 
 (def default-config {:upload-path nil
                      :thumb-path nil
-                     :template-path nil})
+                     :template-path nil
+                     :database {:user "postgres"
+                                :password nil
+                                :host "//localhost:5432/ansel"}})
 
-;; User management ------------------------------------------------------------
+(defn load-config []
+  (let [data (if (exists? "config.json")
+               (parse-string (slurp "config.json") true)
+               default-config)]
+    (dosync
+      (reset! config data)
+      (info "data loaded from disk"))))
+
+(defn assert-fs []
+  (info "creating dirs")
+  (fs/mkdirs (get-uploads-path))
+  (fs/mkdirs (get-thumbs-path)))
+
+(defn init []
+  (load-config)
+  (assert-fs))
+
+;; This init is required when running the server via `lein ring server`
+;; (init)
+
+(defn get-db-spec []
+  {:classname "org.postgresql.Driver"
+   :subprotocol "postgresql"
+   :subname (get-in @config [:database :host])
+   :user (get-in @config [:database :user])})
 
 (defn hash-bcrypt [password]
   (BCrypt/hashpw password (BCrypt/gensalt)))
@@ -39,7 +59,7 @@
   (BCrypt/checkpw p h))
 
 (defn get-user [email]
-  (first (sql-get-user db-spec email)))
+  (first (sql-get-user (get-db-spec) email)))
 
 (defn sql-exists? [q]
   (-> q first :count pos?))
@@ -48,26 +68,24 @@
   (sql-exists? (sql-user-exists db email)))
 
 (defn users-exist? []
-  (sql-exists? (sql-user-count db-spec)))
+  (sql-exists? (sql-user-count (get-db-spec))))
 
 (defn like-exists? [db image user]
   (sql-exists? (sql-like-exists db image user)))
 
 (defn add-user-to-db [user & admin]
   (let [admin (if (and (seq admin) (first admin))
-            true
-            (if (users-exist?)
-              false
-              true))]
-    (with-db-transaction [connection db-spec]
+                true
+                (if (users-exist?)
+                  false
+                  true))]
+    (with-db-transaction [connection (get-db-spec)]
       (when-not (user-exists? connection (:email user))
         (sql-insert-user! connection
                           (hash-bcrypt (:password user))
                           (:email user)
                           (:name user)
                           admin)))))
-
-;; Image management -----------------------------------------------------------
 
 (defn get-thumb-name
   "(get-thumb-name 'dog.jpg' 200)
@@ -94,18 +112,18 @@
 
 (defn get-images [& limit]
   (let [images (if (seq limit)
-                  (sql-get-images-limit db-spec (first limit))
-                  (sql-get-all-images db-spec))]
+                  (sql-get-images-limit (get-db-spec) (first limit))
+                  (sql-get-all-images (get-db-spec)))]
     (map beef-up-image images)))
 
 (defn get-image-by-id 
   ([id] (get-image-by-id id 0))
   ([id user-id]
-   (when-let [images (sql-get-image-by-id db-spec user-id id)]
+   (when-let [images (sql-get-image-by-id (get-db-spec) user-id id)]
       (beef-up-image (first images)))))
 
 (defn add-image-to-db [image user-id]
-  (sql-insert-image! db-spec
+  (sql-insert-image! (get-db-spec)
                      (:filename image)
                      (:caption image)
                      (:focal-length image)
@@ -118,23 +136,23 @@
                      (to-timestamp (:captured image))))
 
 (defn add-image-to-album [image-id album-id]
-  (sql-insert-image-to-album! db-spec image-id album-id))
+  (sql-insert-image-to-album! (get-db-spec) image-id album-id))
 
 (defn add-album-to-db [album user]
   (sql-insert-album!
-    db-spec
+    (get-db-spec)
     (:name album)
     (slugify (:name album))
     (:id user)))
 
 (defn get-all-albums []
-  (sql-get-all-albums db-spec))
+  (sql-get-all-albums (get-db-spec)))
 
 (defn get-images-for-album [album-id]
-  (sql-get-images-for-album db-spec album-id))
+  (sql-get-images-for-album (get-db-spec) album-id))
 
 (defn get-album-by-slug [slug]
-  (sql-get-album-by-slug db-spec slug))
+  (sql-get-album-by-slug (get-db-spec) slug))
 
 (defn get-uploads-path []
   (or (:upload-path @config) "uploads/"))
@@ -146,7 +164,7 @@
   (:template-path @config))
 
 (defn like-image [image user]
-  (with-db-transaction [connection db-spec]
+  (with-db-transaction [connection (get-db-spec)]
     (when-not (like-exists? connection (:id image) (:id user))
       (sql-insert-like! (:id image) (:id user)))))
 
@@ -159,32 +177,13 @@
     (str num-likes " people like this")))
 
 (defn comment-on-image [image user c]
-  (sql-insert-comment! db-spec
+  (sql-insert-comment! (get-db-spec)
                        (:id image)
                        (:id user)
                        c))
 
 (defn get-comments-for-image [image]
-  (sql-get-comments-for-image db-spec (:id image)))
-
-;; Loading --------------------------------------------------------------------
-
-(defn load-data-from-disk []
-  (let [data (if (exists? "config.json")
-               (parse-string (slurp "config.json") true)
-               default-config)]
-    (dosync
-      (reset! config (:config data))
-      (info "data loaded from disk"))))
-
-(defn get-context []
-  {})
-
-(defn assert-fs []
-  (info "creating dirs")
-  (fs/mkdirs (get-uploads-path))
-  (fs/mkdirs (get-thumbs-path)))
-
+  (sql-get-comments-for-image (get-db-spec) (:id image)))
 
 (defn add-cover-to-album [images album]
   (assoc album :cover (or (:cover album)
@@ -210,7 +209,3 @@
 
 (defn add-recent-images [db]
   (assoc db :recent (take 5 (:images db))))
-
-(defn init []
-  (load-data-from-disk)
-  (assert-fs))
