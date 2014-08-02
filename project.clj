707a4c739b6c5a0cf3f36bc85c08c5fdf58d2fb2
Brad Fritz <brad@fewerhassles.com>
2014-08-02 00:10:44 -0400

    Use lein-bower to download JavaScript and CSS assets
    Moves bower dependencies from `bower.json` to `project.clj` and
    adds instructions for installing and running bower to README.

--- project.clj
+++ project.clj
@@ -16,10 +16,16 @@
                  [clj-time "0.7.0"]
                  [jordan "0.2.1"]]
   :license {:name "BSD" :url "http://opensource.org/licenses/BSD-2-Clause"}
+  :bower {:directory "resources/public/bower_components"}
+  :bower-dependencies [[blueimp-file-upload "9.5.7"]
+                       [angular "1.3.0-beta.13"]
+                       [bootstrap "3.2.0"]]
   :jvm-opts ["-Xmx1g" "-Djava.awt.headless=true"]
   :ring {:handler ansel.server/server
          :init ansel.db/init}
-  :plugins [[lein-ring "0.8.2"]]
+  :plugins [[lein-ring "0.8.2"]
+            [lein-bower "0.5.1"]
+            [lein-npm "0.4.0"]]
   :main ^:skip-aot ansel.core
   :target-path "target/%s"
   :jar-exclusions [#"\.cljx|\.DS_Store"]
